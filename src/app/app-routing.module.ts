9914f79f642fde4631b1f353e51cb957d44e2c49
Anantha Ramanathan A <anantharamanathan138@gmail.com>
2021-09-28 13:27:36 +0530

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './component/home/home.component';
+import { HeaderComponent } from './component/header/header.component';
+import { FooterComponent } from './component/footer/footer.component';
+import { EducationComponent } from './component/education/education.component';
+import { CertificationsComponent } from './component/certifications/certifications.component';
+import { ContactMeComponent } from './component/contact-me/contact-me.component';
+import { ContributionsComponent } from './component/contributions/contributions.component';
+import { ExperienceComponent } from './component/experience/experience.component';
+import { ProjectsComponent } from './component/projects/projects.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HeaderComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'education', component: EducationComponent },
+  { path: 'contributions', component: ContributionsComponent },
+  { path: 'certifications', component: CertificationsComponent },
+  { path: 'projects', component: ProjectsComponent },
+  { path: 'contactMe', component: ContactMeComponent },
+  { path: 'experience', component: ExperienceComponent }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './component/header/header.component';
+import { HomeComponent } from './component/home/home.component';
+import { FooterComponent } from './component/footer/footer.component';
+import { EducationComponent } from './component/education/education.component';
+import { ExperienceComponent } from './component/experience/experience.component';
+import { ProjectsComponent } from './component/projects/projects.component';
+import { ContributionsComponent } from './component/contributions/contributions.component';
+import { CertificationsComponent } from './component/certifications/certifications.component';
+import { ContactMeComponent } from './component/contact-me/contact-me.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    FooterComponent,
+    EducationComponent,
+    ExperienceComponent,
+    ProjectsComponent,
+    ContributionsComponent,
+    CertificationsComponent,
+    ContactMeComponent
   ],
   imports: [
     BrowserModule,
